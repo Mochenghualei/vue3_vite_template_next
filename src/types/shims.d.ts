48ebfa4a4a1eb74fd4936eefb091e8191bc0f10b
mocheng <18830279823@163.com>
2023-06-07 10:19:27 +0800

    update: 新增环境变量ts提示

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -2,6 +2,20 @@ declare interface Window {
   // extend the window
 }
 
+interface ImportMetaEnv {
+  readonly VITE_ENV: string,
+  readonly VITE_DSC: string,
+  readonly VITE_APP_TRUE_API: string,
+  readonly VITE_APP_BASE_API: string,
+  readonly VITE_APP_REQUEST_API: string,
+  readonly VITE_APP_TITLE: string,
+  readonly VITE_APP_DESCRIPTION: string,
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.vue' {
   import { type DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
